39dfc2fbf54ea251e4f10d356cd2835068e262ac
Noel <anufreos@gmail.com>
2019-01-15 13:49:08 +0000

    durable messages

--- send.py
+++ send.py
@@ -10,7 +10,7 @@ channel.queue_declare(queue='task_queue', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "Hello world"
 
-channel.basic_publish(exchange='', routing_key='task_queue', body=message)
+channel.basic_publish(exchange='', routing_key='task_queue', body=message, properties=pika.BasicProperties(delivery_mode = 2,))
 
 print ("[x] Sent  %r" % message)
 
